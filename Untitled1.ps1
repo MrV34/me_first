8b39f1bae7b88a0b8c81d1d5c393dd4f474faec3
MrV34 <peter.persson@hogia.se>
2017-11-23 14:43:51 +0100

    Me_second

---
+++ Untitled1.ps1
@@ -0,0 +1,9 @@
+﻿ Param(
+                [Parameter(Mandatory=$True,Position=1)]
+                [string]$node,
+                [Parameter(Mandatory=$True)]
+                [string]$status,
+                [switch]$force = $false
+                )
+
+                echo "$node har $status"
